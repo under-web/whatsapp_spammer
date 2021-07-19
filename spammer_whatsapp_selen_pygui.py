32419b29a56d08adde77c2be402175f711818496
Artem <53917645+under-web@users.noreply.github.com>
2021-07-19 22:58:29 +0300

    refactor

--- spammer_whatsapp_selen_pygui.py
+++ spammer_whatsapp_selen_pygui.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 import random
-
+# TODO: переписать весь код
 
 # v.2.1
 # ============================
@@ -11,8 +11,9 @@ import random
 
 def get_phone():
     with open(r'T:\Desktop\tg700.txt', 'r') as file:                     # место с телефонами
-        phones = file.readlines()
-        set_phone = set(phones)
+        set_phone = file.readlines()
+        # set_phone = set(phones)
+        print(len(set_phone))
     return set_phone
 
 # TODO: сделать скрытой отправку сообщений
@@ -27,38 +28,38 @@ def run_spammer(set_phone):
 
     for phone in set_phone:
         print(phone, 'В списке №{} из {}'.format(step, all_phone))
-        step += 1
-        if step % 50 == 0:                                          # если делится без остатка на 50 пауза 15 мин
-            time.sleep(900)
-        else:
-            try:
-                driver.execute_script("window.open('https://web.whatsapp.com/send?phone={}')".format(phone.strip()))
-            except Exception as e:
-                print('ошибка с урл', e)
-                continue
+        # step += 1
+        # if step % 50 == 0:                                          # если делится без остатка на 50 пауза 15 мин
+        #     time.sleep(900)
+        # else:
+        try:
+            driver.execute_script("window.open('https://web.whatsapp.com/send?phone={}')".format(phone.strip()))
+        except Exception as e:
+            print('ошибка с урл', e)
+            continue
 
-            try:
-                time.sleep(random.randint(5, 10))
-                with open('messages.txt', 'r') as files: # открываем файл с сообщениями
-                    txt = files.readlines()              # считываем его в список
-                driver.switch_to.window(driver.window_handles[-1])
+        try:
+            time.sleep(random.randint(5, 10))
+            with open('messages.txt', 'r') as files: # открываем файл с сообщениями
+                txt = files.readlines()              # считываем его в список
+            driver.switch_to.window(driver.window_handles[-1])
 
 
 
-                msg_box = driver.find_elements_by_class_name('_2_1wd')  # находим бокс куда вставлять сообщение
-                msg_box[1].send_keys(random.choice(txt))  # вставляем нужный текст выбрав рандомно из списка
-                time.sleep(random.randint(3, 5))
+            msg_box = driver.find_elements_by_class_name('_2_1wd')  # находим бокс куда вставлять сообщение
+            msg_box[1].send_keys(random.choice(txt))  # вставляем нужный текст выбрав рандомно из списка
+            time.sleep(random.randint(3, 5))
 
-                button = driver.find_element_by_class_name('_1E0Oz')  # находим кнопку
-                time.sleep(1)
-                button.click()  # кликаем по ней
-                time.sleep(2)
-                print('Отправил')
+            button = driver.find_element_by_class_name('_1E0Oz')  # находим кнопку
+            time.sleep(1)
+            button.click()  # кликаем по ней
+            time.sleep(2)
+            print('Отправил')
 
-                time.sleep(random.randint(45, 80))
+            time.sleep(random.randint(45, 80))
 
-            except Exception as ero:
-                print('Что то пошло не так', ero)
+        except Exception as ero:
+            print('Что то пошло не так', ero)
 
 
 
